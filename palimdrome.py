3a5a0c615b6e7063a3f8246dd941b013bbf72ac7
Sam LoCascio <sflocascio@gmail.com>
2018-11-05 12:10:42 -0500

    update of palimdrome project that wasn't originally committed

--- palimdrome.py
+++ palimdrome.py
@@ -1,34 +1,47 @@
 #You have to write a program that, when run, asks the user to input some text. It can be a phrase, a sentence, or multiple sentences. After it is entered, your program will let the user know if it is a palindrome or not. Use "is a palindrome" and "is not a palindrome" in your output in order for the tests to pass.
 #Letter casing and punctuation do not matter when testing a palindrome. All of the following are valid palindromes:
 
-word = str(input("Enter word or sentence here to see if it's a paladrome"))
+def main_page():
 
-def clean_text(text):
-   """given a text return the text with no punctiation"""
-   # text = word.lower()
-   # text = word.replace(" ", "")
-   # return text
-    for char in word:
-        if char.isalpha():
-        new_text = new_text + char
-    return new_text
+    word_1 = str(input("Enter word or sentence here to see if it's a paladrome"))
 
-def is_a_paladrome(chars):
-    """check to see if this is a palidrome"""
-    text = chars.text
-    if len(text) <= 1:
-        print (True)
-    elif text[0] == text[-1]:
-        return is_a_paladrome (text[1:-1])
+    def clean_text(word):
+        """given a text return the text with no punctiation"""
+        word = word.lower()
+        new_text = ""
+        for char in word:
+            if char.isalpha():
+                new_text = new_text + char
+        return new_text
+
+    new = clean_text(word_1)
+    print (new)  
+
+    def is_a_paladrome(chars):
+        """check to see if this is a palidrome"""
+        
+        if len(chars) <= 1:
+            return True
+        elif chars[0] == chars[-1]:
+            return is_a_paladrome (chars[1:-1])
+        else:
+            return False
+
+    if is_a_paladrome(new) == True:
+        print("is a palindrone")
+
     else:
-        print(False)
+        print("not a palindrone")
 
-if is_a_paladrome(text) == True:
-    print("is a palindrone")
+    again = (input("would you like to try again? Y for yes"))
+    if again == 'Y':
+        main_page()
+    else:
+        print("goodbye")
+   
 
-else:
-    print("not a palindrone")
 
+main_page()
 
 
 
